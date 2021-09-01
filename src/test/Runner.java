70d38bbe752fe6bdba1089c1ca28af53f4406322
Nick D <nickduncanson@gmail.com>
2021-09-01 12:12:26 -0400

    September 1st Change

---
+++ src/test/Runner.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+		double number1=100.2507;
+
+		System.out.println("Number 1: " + number1);
+
+	}
+
+}
